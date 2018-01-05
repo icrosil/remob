7398f7aa7c14b952c7413d0340bdad31cce9ff01
Illia Olenchenko <icrosil@gmail.com>
2018-01-05 21:38:36 +0200

    remob added simple implementation for reducer

--- src/Reducer.js
+++ src/Reducer.js
@@ -1,11 +1,11 @@
-class Reducer {
+export default class Reducer {
   state = {};
   constructor(initialState = this.state) {
     this.state = initialState;
   }
-  get reducer() {
-    // TODO implement
-  }
+  // TODO implementation
+  // check every action in this instance and change state
+  // TODO check how namespacing could be achieved
+  // TODO bind reducer in constructor
+  reducer = (state = this.state) => state;
 }
-
-export default Reducer;
