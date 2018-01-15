d4719eff65f654e833ec151df3059f741f15a971
icrosil <icrosil@gmail.com>
2018-01-15 18:17:34 +0200

    remob changed name for state/initialState

--- src/Reducer.js
+++ src/Reducer.js
@@ -3,17 +3,17 @@ export default class Reducer {
   // TODO make this fields not changable from outer world
   // TODO should it be here or in proto? describe somewhere why to do so and find another option
   // TODO describe how actions settled
-  state = {};
+  initialState = {};
   // TODO check do i need this state setter, would it be useful?
-  constructor(initialState = this.state) {
-    this.state = initialState;
+  constructor(initialState = this.initialState) {
+    this.initialState = initialState;
   }
   // TODO implementation
   // check every action in this instance and change state
   // TODO check how namespacing could be achieved
   // TODO bind reducer in constructor
   // TODO i guess we should make it static
-  reducer = (state = this.state, action) => {
+  reducer = (state = this.initialState, action) => {
     const callableAction = this.actions[action.type];
     // TODO investigate spacenaming issue
     // TODO investigate logic of using immer inside
