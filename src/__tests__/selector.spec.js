a51d49bbfcfa3ca0374b9ed87842e1c64566994c
Illia Olenchenko <icrosil@gmail.com>
2018-02-05 00:26:00 +0200

    remob added spec for selector

---
+++ src/__tests__/selector.spec.js
@@ -0,0 +1,44 @@
+import selector from '../selector';
+import { validate } from '../util/decorator';
+import Reducer from '../Reducer';
+
+jest.mock('../util/decorator', () => ({
+  validate: jest.fn(() => {
+    const error = { value: 'issue' };
+    throw error;
+  }),
+}));
+
+jest.mock('../Reducer', () => class ClassName {
+  ['registerSelector'] = jest.fn();
+});
+
+describe('selector', () => {
+  const instance = new Reducer();
+  const ref = {
+    value: jest.fn(),
+  };
+
+  test('should be a function', () => {
+    expect(selector).toEqual(expect.any(Function));
+  });
+
+  test('should call decorator validation', () => {
+    try {
+      selector(Reducer, 'method', ref);
+    } catch (e) {
+      expect(validate).toHaveBeenCalledWith(Reducer, ref.value, 'method', 'action');
+    }
+  });
+
+  test('should registrate everything', () => {
+    validate.mockReset();
+    const result = selector(instance, 'method', ref);
+    expect(instance.registerSelector).toHaveBeenCalledWith(expect.any(Function), 'method');
+    expect(result).toEqual({
+      configurable: undefined,
+      enumerable: undefined,
+      get: expect.any(Function),
+    });
+  });
+});
