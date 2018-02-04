0f1046a3656041d96378589cef89b41a984508c8
Illia Olenchenko <icrosil@gmail.com>
2018-02-04 21:54:11 +0200

    remob added simple test and travis config

---
+++ src/__tests__/index.spec.js
@@ -0,0 +1,12 @@
+import * as remob from '../';
+
+describe('index', () => {
+  test('should export all features', () => {
+    expect(remob).toHaveProperty('action');
+    expect(remob).toHaveProperty('combineRemob');
+    expect(remob).toHaveProperty('inject');
+    expect(remob).toHaveProperty('Reducer');
+    expect(remob).toHaveProperty('selector');
+    expect(remob).toHaveProperty('thunk');
+  });
+});
