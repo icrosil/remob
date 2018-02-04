f2753b45692a8e1b50689f63ad2225e07da2694d
Illia Olenchenko <icrosil@gmail.com>
2018-02-05 01:05:28 +0200

    remob added spec for combineRemob

---
+++ src/__tests__/combineRemob.spec.js
@@ -0,0 +1,17 @@
+import { combineReducers } from 'redux';
+import { Reducer, combineRemob } from '../';
+
+jest.mock('redux', () => ({
+  combineReducers: jest.fn(),
+}));
+
+describe('combineRemob', () => {
+  const instance = new Reducer();
+  test('should be a function', () => {
+    expect(combineRemob).toEqual(expect.any(Function));
+  });
+  test('should map all remobs and call combineReducers', () => {
+    combineRemob({ instance, notRemob: () => {} });
+    expect(combineReducers).toHaveBeenCalled();
+  });
+});
