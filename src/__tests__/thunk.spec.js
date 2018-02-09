10979082cbfda431599f7d18535e7a750d0a3511
Illia Olenchenko <icrosil@gmail.com>
2018-02-09 22:02:23 +0200

    remob added thunk specs

--- src/__tests__/thunk.spec.js
+++ src/__tests__/thunk.spec.js
@@ -1,7 +1,7 @@
 import thunk from '../thunk';
 import action from '../action';
 
-jest.mock('../action', () => jest.fn(() => () => {}));
+jest.mock('../action', () => jest.fn((method, dispatch) => () => dispatch));
 
 describe('thunk', () => {
   test('should be a function', () => {
@@ -10,5 +10,21 @@ describe('thunk', () => {
 
   test('should call action on init', () => {
     expect(action).toHaveBeenCalledWith(undefined, expect.any(Function));
+  });
+
+  describe('dispatcher', () => {
+    test('should be a function with 2 params', () => {
+      const dispatch = thunk();
+      expect(dispatch).toEqual(expect.any(Function));
+      expect(dispatch).toHaveLength(2);
+    });
+    test('should pass 2 param into dispatch', () => {
+      const dispatch = jest.fn();
+      const dispatchable = thunk()(1, 2);
+      expect(dispatchable).toEqual(expect.any(Function));
+      expect(dispatchable).toHaveLength(1);
+      dispatchable(dispatch);
+      expect(dispatch).toHaveBeenCalledWith(2);
+    });
   });
 });
