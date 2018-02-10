74f3b208f958a131233a354877f905bef15e0284
Illia Olenchenko <icrosil@gmail.com>
2018-02-10 12:59:58 +0200

    remob fixed build preset

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,7 @@
 module.exports = {
+  transform: {
+    '^.+\\.js$': 'babel-jest',
+  },
   collectCoverage: true,
   coverageThreshold: {
     global: {
--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,6 @@ import nodeResolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
 import replace from 'rollup-plugin-replace';
 import uglify from 'rollup-plugin-uglify';
-import babelrc from 'babelrc-rollup';
 import commonjs from 'rollup-plugin-commonjs';
 
 const config = {
